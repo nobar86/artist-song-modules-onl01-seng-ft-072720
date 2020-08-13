201f3e4c07f6fdebbd099a840593f48c650bf2fa
Michael Rabon <nobar86@gmail.com>
2020-08-13 00:00:10 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,12 @@
 module Memorable
   
-    def self.reset_all
-    self.all.clear
+    def reset_all
+      all.clear
   end
   
   
-   def self.count
-    self.all.count
+   def count
+     all.count
   end
   
 end
